108f9abf9957d54d909e00cc124ac5594382cc84
salmonindaeyo <iamfhaa@hotmail.com>
2023-11-21 02:41:43 +0700

    Update headerbar user detail picture

--- src/pages/MainPages/HeadBar/User.js
+++ src/pages/MainPages/HeadBar/User.js
@@ -38,7 +38,7 @@ import { useMediaQuery } from 'react-responsive'
                 <div className='font-medium text-[10px]'>Edit Profile</div>
             </div>
            } 
-            <img className='min-w-[40px]' src={process.env.PUBLIC_URL + '/friend-pic.png'} />
+            <img className='w-[50px] h-[45px] object-cover rounded-md' src={userDetails.image} />
         </div>
         <Popover
         id={id}
