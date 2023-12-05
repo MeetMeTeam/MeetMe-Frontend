61fa94f0604cd0ff8be00300594c0b41e17324d5
salmonindaeyo <iamfhaa@hotmail.com>
2023-12-05 22:38:26 +0700

    Create loading room

--- src/pages/MainPages/SideBar.js
+++ src/pages/MainPages/SideBar.js
@@ -1,35 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { styled } from "@mui/system";
 import MainPageButton from "./SideBar/MainPageButton";
 import { connect } from "react-redux";
 import ActiveRoomButton from "./SideBar/ActiveRoomButton";
-import SafetyDividerIcon from '@mui/icons-material/SafetyDivider';
-import styles from "../../shared/css/mainPage.module.css"
-
+import SafetyDividerIcon from "@mui/icons-material/SafetyDivider";
+import styles from "../../shared/css/mainPage.module.css";
+import Loading from "../../shared/components/Loading";
 
 const SideBar = ({ activeRooms, isUserInRoom }) => {
+  const [isShowLoading, setIsisShowLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsisShowLoading(false);
+    }, 1500);
+  }, []);
+
   return (
-    <div className={`bg-purple-60 w-full rounded-2xl px-4 h-full max-w-[822px] ${ activeRooms.length === 0 ? " justify-between ": " justify-start "} flex flex-col  ${styles.container}` }>
-     <div className="text-white text-[32px] font-bold py-5">Room </div> 
-      {activeRooms && activeRooms.map((room) => (
-        <ActiveRoomButton
-          roomId={room.roomId}
-          creatorUsername={room.creatorUsername}
-          amountOfParticipants={room.participants.length}
-          key={room.roomId}
-          isUserInRoom={isUserInRoom}
-          roomName={room.roomCreator.roomName}
-          type={room.roomCreator.type}
-          data={room}
-        />
-        
-      ))}
-      {activeRooms.length === 0 && 
-      <div className={ `w-full flex justify-center items-center h-full flex-col`}>
-        <SafetyDividerIcon className="text-white" sx={{ fontSize: "43px" }}/>
-        <div className="text-white">No room active now . Create room to invite friends !</div>
-        </div>}
-        <div/>
+    <div
+      className={`bg-purple-60 w-full rounded-2xl px-4 h-full max-w-[822px] ${
+        activeRooms.length === 0 ? " justify-between " : " justify-start "
+      } flex flex-col  ${styles.container}`}
+    >
+      <div className="text-white text-[32px] font-bold py-5">Room </div>
+      {activeRooms &&
+        activeRooms.map((room) => (
+          <ActiveRoomButton
+            roomId={room.roomId}
+            creatorUsername={room.creatorUsername}
+            amountOfParticipants={room.participants.length}
+            key={room.roomId}
+            isUserInRoom={isUserInRoom}
+            roomName={room.roomCreator.roomName}
+            type={room.roomCreator.type}
+            data={room}
+          />
+        ))}
+
+      {activeRooms.length === 0 && (
+        <div
+          className={`w-full flex justify-center items-center h-full flex-col`}
+        >
+          {isShowLoading && <Loading />}
+          {!isShowLoading && (
+            <div className="w-full flex justify-center items-center h-full flex-col">
+              <SafetyDividerIcon
+                className="text-white"
+                sx={{ fontSize: "43px" }}
+              />
+              <div className="text-white">
+                No room active now . Create room to invite friends !
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+      <div />
     </div>
   );
 };
