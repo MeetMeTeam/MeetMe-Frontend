e77a953896e472fc501d96ed5769f81c1b257546
salmonindaeyo <iamfhaa@hotmail.com>
2023-12-05 21:12:46 +0700

    Update banner responsive

--- src/pages/MainPages/BannerAdvert/BannerAdvert.js
+++ src/pages/MainPages/BannerAdvert/BannerAdvert.js
@@ -15,20 +15,20 @@ const BannerAdvert = () => {
    setInterval(changeImage, 2000);
   return (
     <div className="max-w-[822px]">
-        <div className=" h-[150px] rounded-2xl relative flex justify-center items-center">
-        <img className=' z-40 absolute w-[140px] top-[-60px] left-[-20px]' src={process.env.PUBLIC_URL + '/logo_banner.svg'} />
+        <div className=" h-fit rounded-2xl relative flex justify-center items-center">
+        <img className=' z-40 absolute lg:w-[140px] w-[70px] lg:top-[-100px]  lg:left-[-50px] top-[-40px] left-[-30px]' src={process.env.PUBLIC_URL + '/logo_banner.svg'} />
         <div 
         className="bg-yellow-80 absolute
-         py-2 px-8 drop-shadow-lg left-[85px]
-         top-[-20px]
-         font-bold text-[24px] rounded-2xl text-black
+         py-2 px-8 drop-shadow-lg lg:left-[55px]
+         lg:top-[-30px] top-[-20px] left-[25px]
+         font-bold lg:text-[24px] text-[10px] rounded-2xl text-black
          z-40
          "
          >
           News
           </div>
 
-          <img className=' h-[150px]  rounded-2xl object-contain top-[-60px] left-[-20px] z-20' 
+          <img className='rounded-2xl object-contain top-[-60px] left-[-20px] z-20' 
           src={process.env.PUBLIC_URL + '/Banner-test.png'} />
 
         </div>
