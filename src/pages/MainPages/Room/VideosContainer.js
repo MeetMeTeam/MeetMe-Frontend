5d4214909ce68ad5b8e2ba20f7586d963631c147
Nuttawat Rodsomboob <nuttawat.rod@innovasive.co.th>
2024-02-14 15:12:05 +0700

    [create] avtar in room

--- src/pages/MainPages/Room/VideosContainer.js
+++ src/pages/MainPages/Room/VideosContainer.js
@@ -5,7 +5,7 @@ import { connect } from "react-redux";
 import Video from "./Video";
 import { useSelector } from "react-redux";
 import * as socketConnection from "../../../realtimeCommunication/socketConnection";
-
+import AvatarUserPreview from "./AvatarUserPreview";
 const MainContainer = styled("div")({
   height: "85%",
   width: "100%",
@@ -69,13 +69,18 @@ const VideosContainer = ({
     const image = otherUserActionCam.find(
       (item) => item.userId === stream.id
     )?.image;
-
+    const id = otherUserActionCam.find(
+      (item) => item.userId === stream.id
+    )?.userId;
+    console.log(id);
     return (
       <div
         key={stream.remoteStream.id}
         className="flex flex-col justify-center items-center relative p-10"
       >
-                <div className="bg-white py-10 opacity-30 rounded-2xl w-full z-[0] h-full absolute">  </div>
+        <div className="bg-white py-10 opacity-30 rounded-2xl w-full z-[0] h-full absolute">
+          {" "}
+        </div>
 
         {isCameraEnabled || otherUserActionCam.length === 0 ? (
           <Video
@@ -90,11 +95,12 @@ const VideosContainer = ({
               stream={stream.remoteStream}
               id={stream.connUserSocketId}
             />
-            <img
+            {/* <img
               src={image}
               className="rounded-full object-cover drop-shadow-xl border-4 border-white"
               style={{ width: "150px", height: "150px" }}
-            />
+            /> */}
+            <AvatarUserPreview id={id} />
           </div>
         )}
         {participants.map((item) => (
@@ -117,22 +123,24 @@ const VideosContainer = ({
   return (
     <div className="absolute top-1/4 gap-4 w-full grid lg:grid-cols-4 grid-cols-2  sm:px-12">
       <div className="flex flex-col justify-center items-center relative p-10">
-        <div className="bg-white py-10 opacity-30 rounded-2xl w-full h-full absolute">  </div>
+        <div className="bg-white py-10 opacity-30 rounded-2xl w-full h-full absolute">
+          {" "}
+        </div>
         {cameraEnabled ? (
           <Video size="300px" stream={localStream} isLocalStream />
         ) : (
           <div>
             <Video size="1px" stream={localStream} isLocalStream />
-            <img
+            {/* <img
               src={myImage}
               className="rounded-full object-cover drop-shadow-xl border-4 border-white"
               style={{ width: "150px", height: "150px" }}
-            />
+            /> */}
+            <AvatarUserPreview id={userId} />
           </div>
         )}
         <div className="mt-4 font-bold drop-shadow-md bg-purple-60 text-white px-2 py-1 rounded-md">
           {"Me"}
-          
         </div>
       </div>
 
